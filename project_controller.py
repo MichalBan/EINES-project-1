64950529eec80e347308465e17915e27a853cf43
MichalBan <michal.banasik.9@gmail.com>
2023-04-19 16:48:49 +0200

    intent attempt

---
+++ project_controller.py
@@ -0,0 +1,536 @@
+# The program implements a simple controller for a network with 6 hosts and 5 switches. The switches are connected in
+# a diamond topology (without vertical links): - 3 hosts are connected to the left (s1) and 3 to the right (s5) edge
+# of the diamond. Overall operation of the controller: - default routing is set in all switches on the reception of
+# packet_in messages form the switch, - then the routing for (h1-h4) pair in switch s1 is changed every one second in
+# a round-robin manner to load balance the traffic through switches s3, s4, s2.
+
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.util import dpidToStr
+from pox.lib.addresses import IPAddr, EthAddr
+from pox.lib.packet.arp import arp
+from pox.lib.packet.ethernet import ethernet, ETHER_BROADCAST
+from pox.lib.packet.packet_base import packet_base
+from pox.lib.packet.packet_utils import *
+import pox.lib.packet as pkt
+from pox.lib.recoco import Timer
+import time
+
+log = core.getLogger()
+
+s1_dpid = 0
+s2_dpid = 0
+s3_dpid = 0
+s4_dpid = 0
+s5_dpid = 0
+
+s1_p1 = 0
+s1_p4 = 0
+s1_p5 = 0
+s1_p6 = 0
+s2_p1 = 0
+s3_p1 = 0
+s4_p1 = 0
+
+pre_s1_p1 = 0
+pre_s1_p4 = 0
+pre_s1_p5 = 0
+pre_s1_p6 = 0
+pre_s2_p1 = 0
+pre_s3_p1 = 0
+pre_s4_p1 = 0
+
+turn = 0
+
+
+class SwitchInfo:
+    s1_port = 0
+    num_flows = 0
+    delay = 0
+
+    def __init__(self, new_s1_port, new_num_flows, new_delay):
+        self.s1_port = new_s1_port
+        self.num_flows = new_num_flows
+        self.delay = new_delay
+
+
+class SwitchInfoList:
+    switch_infos = [SwitchInfo(4, 3, 200), SwitchInfo(5, 4, 50), SwitchInfo(6, 2, 10)]
+
+    def sort_by_flows(self):
+        def get_flows(e):
+            return e.num_flows
+
+        self.switch_infos.sort(key=get_flows)
+
+
+class Intent:
+    source = 0  # 1 for h1, 2 for h2, 3 for h3
+    destination = 0  # 4 for h4, 5 for h5, 6 for h6
+    max_delay = 0
+
+    def __init__(self, new_source, new_destination, new_delay):
+        self.source = new_source
+        self.destination = new_destination
+        self.max_delay = new_delay
+
+    def print_self(self):
+        print("Intent: max delay from h{} to h{} must not exceed {}".format(self.source, self.destination,
+                                                                            self.max_delay))
+
+
+TheSwitchInfoList = SwitchInfoList()
+active_intents = []
+
+
+def print_intents():
+    for intent in active_intents:
+        intent.print_self()
+
+
+def _timer_func():
+    global s1_dpid, s2_dpid, s3_dpid, s4_dpid, s5_dpid, turn
+    core.openflow.getConnection(s1_dpid).send(of.ofp_stats_request(body=of.ofp_port_stats_request()))
+    core.openflow.getConnection(s2_dpid).send(of.ofp_stats_request(body=of.ofp_port_stats_request()))
+    core.openflow.getConnection(s3_dpid).send(of.ofp_stats_request(body=of.ofp_port_stats_request()))
+    core.openflow.getConnection(s4_dpid).send(of.ofp_stats_request(body=of.ofp_port_stats_request()))
+
+
+def getTheTime():  # function to create a timestamp
+    flock = time.localtime()
+    then = "[%s-%s-%s" % (str(flock.tm_year), str(flock.tm_mon), str(flock.tm_mday))
+
+    if int(flock.tm_hour) < 10:
+        hrs = "0%s" % (str(flock.tm_hour))
+    else:
+        hrs = str(flock.tm_hour)
+    if int(flock.tm_min) < 10:
+        mins = "0%s" % (str(flock.tm_min))
+    else:
+        mins = str(flock.tm_min)
+
+    if int(flock.tm_sec) < 10:
+        secs = "0%s" % (str(flock.tm_sec))
+    else:
+        secs = str(flock.tm_sec)
+
+    then += "]%s.%s.%s" % (hrs, mins, secs)
+    return then
+
+
+def _handle_portstats_received(event):
+    # Observe the handling of port statistics provided by this function.
+
+    global s1_dpid, s2_dpid, s3_dpid, s4_dpid, s5_dpid
+    global s1_p1, s1_p4, s1_p5, s1_p6, s2_p1, s3_p1, s4_p1
+    global pre_s1_p1, pre_s1_p4, pre_s1_p5, pre_s1_p6, pre_s2_p1, pre_s3_p1, pre_s4_p1
+
+    if event.connection.dpid == s1_dpid:  # The DPID of one of the switches involved in the link
+        for f in event.stats:
+            if int(f.port_no) < 65534:
+                if f.port_no == 1:
+                    pre_s1_p1 = s1_p1
+                    s1_p1 = f.rx_packets
+                    # print "s1_p1->","TxDrop:", f.tx_dropped,"RxDrop:",f.rx_dropped,"TxErr:",f.tx_errors,"CRC:",f.rx_crc_err,"Coll:",f.collisions,"Tx:",f.tx_packets,"Rx:",f.rx_packets
+                if f.port_no == 4:
+                    pre_s1_p4 = s1_p4
+                    s1_p4 = f.tx_packets
+                    # s1_p4=f.tx_bytes
+                    # print "s1_p4->","TxDrop:", f.tx_dropped,"RxDrop:",f.rx_dropped,"TxErr:",f.tx_errors,"CRC:",f.rx_crc_err,"Coll:",f.collisions,"Tx:",f.tx_packets,"Rx:",f.rx_packets
+                if f.port_no == 5:
+                    pre_s1_p5 = s1_p5
+                    s1_p5 = f.tx_packets
+                if f.port_no == 6:
+                    pre_s1_p6 = s1_p6
+                    s1_p6 = f.tx_packets
+
+    if event.connection.dpid == s2_dpid:
+        for f in event.stats:
+            if int(f.port_no) < 65534:
+                if f.port_no == 1:
+                    pre_s2_p1 = s2_p1
+                    s2_p1 = f.rx_packets
+                    # s2_p1=f.rx_bytes
+        print(getTheTime(), "s1_p4(Sent):", (s1_p4 - pre_s1_p4), "s2_p1(Received):", (s2_p1 - pre_s2_p1))
+
+    if event.connection.dpid == s3_dpid:
+        for f in event.stats:
+            if int(f.port_no) < 65534:
+                if f.port_no == 1:
+                    pre_s3_p1 = s3_p1
+                    s3_p1 = f.rx_packets
+        print(getTheTime(), "s1_p5(Sent):", (s1_p5 - pre_s1_p5), "s3_p1(Received):", (s3_p1 - pre_s3_p1))
+
+    if event.connection.dpid == s4_dpid:
+        for f in event.stats:
+            if int(f.port_no) < 65534:
+                if f.port_no == 1:
+                    pre_s4_p1 = s4_p1
+                    s4_p1 = f.rx_packets
+        print(getTheTime(), "s1_p6(Sent):", (s1_p6 - pre_s1_p6), "s4_p1(Received):", (s4_p1 - pre_s4_p1))
+
+
+def _handle_ConnectionUp(event):
+    # waits for connections from all switches, after connecting to all of them it starts a round robin timer for triggering h1-h4 routing changes
+    global s1_dpid, s2_dpid, s3_dpid, s4_dpid, s5_dpid
+    print("ConnectionUp: ", dpidToStr(event.connection.dpid))
+
+    # remember the connection dpid for the switch
+    for m in event.connection.features.ports:
+        if m.name == "s1-eth1":
+            # s1_dpid: the DPID (datapath ID) of switch s1;
+            s1_dpid = event.connection.dpid
+            print("s1_dpid=", s1_dpid)
+        elif m.name == "s2-eth1":
+            s2_dpid = event.connection.dpid
+            print("s2_dpid=", s2_dpid)
+        elif m.name == "s3-eth1":
+            s3_dpid = event.connection.dpid
+            print("s3_dpid=", s3_dpid)
+        elif m.name == "s4-eth1":
+            s4_dpid = event.connection.dpid
+            print("s4_dpid=", s4_dpid)
+        elif m.name == "s5-eth1":
+            s5_dpid = event.connection.dpid
+            print("s5_dpid=", s5_dpid)
+
+    if s1_dpid != 0 and s2_dpid != 0 and s3_dpid != 0 and s4_dpid != 0 and s5_dpid != 0:
+        Timer(1, _timer_func, recurring=True)
+
+
+def _handle_PacketIn(event):
+    global s1_dpid, s2_dpid, s3_dpid, s4_dpid, s5_dpid, active_intents
+
+    packet = event.parsed
+    # print "_handle_PacketIn is called, packet.type:", packet.type, " event.connection.dpid:", event.connection.dpid
+
+    # Below, set the default/initial routing rules for all switches and ports.
+    # All rules are set up in a given switch on packet_in event received from the switch which means no flow entry has been found in the flow table.
+    # This setting up may happen either at the very first pactet being sent or after flow entry expirationn inn the switch
+
+    if event.connection.dpid == s1_dpid:
+        handle_s1(event, packet)
+
+    if event.connection.dpid == s2_dpid:
+        msg = of.ofp_flow_mod()
+        msg.priority = 10
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.in_port = 1
+        msg.match.dl_type = 0x0806  # rule for ARP packets (x0806)
+        msg.actions.append(of.ofp_action_output(port=2))
+        event.connection.send(msg)
+
+        msg = of.ofp_flow_mod()
+        msg.priority = 10
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.in_port = 1
+        msg.match.dl_type = 0x0800
+        msg.actions.append(of.ofp_action_output(port=2))
+        event.connection.send(msg)
+
+        msg = of.ofp_flow_mod()
+        msg.priority = 10
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.in_port = 2
+        msg.match.dl_type = 0x0806
+        msg.actions.append(of.ofp_action_output(port=1))
+        event.connection.send(msg)
+
+        msg = of.ofp_flow_mod()
+        msg.priority = 10
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.in_port = 2
+        msg.match.dl_type = 0x0800
+        msg.actions.append(of.ofp_action_output(port=1))
+        event.connection.send(msg)
+
+    elif event.connection.dpid == s3_dpid:
+        msg = of.ofp_flow_mod()
+        msg.priority = 10
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.in_port = 1
+        msg.match.dl_type = 0x0806
+        msg.actions.append(of.ofp_action_output(port=2))
+        event.connection.send(msg)
+
+        msg = of.ofp_flow_mod()
+        msg.priority = 10
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.in_port = 1
+        msg.match.dl_type = 0x0800
+        msg.actions.append(of.ofp_action_output(port=2))
+        event.connection.send(msg)
+
+        msg = of.ofp_flow_mod()
+        msg.priority = 10
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.in_port = 2
+        msg.match.dl_type = 0x0806
+        msg.actions.append(of.ofp_action_output(port=1))
+        event.connection.send(msg)
+
+        msg = of.ofp_flow_mod()
+        msg.priority = 10
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.in_port = 2
+        msg.match.dl_type = 0x0800
+        msg.actions.append(of.ofp_action_output(port=1))
+        event.connection.send(msg)
+
+    elif event.connection.dpid == s4_dpid:
+        msg = of.ofp_flow_mod()
+        msg.priority = 10
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.in_port = 1
+        msg.match.dl_type = 0x0806
+        msg.actions.append(of.ofp_action_output(port=2))
+        event.connection.send(msg)
+
+        msg = of.ofp_flow_mod()
+        msg.priority = 10
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.in_port = 1
+        msg.match.dl_type = 0x0800
+        msg.actions.append(of.ofp_action_output(port=2))
+        event.connection.send(msg)
+
+        msg = of.ofp_flow_mod()
+        msg.priority = 10
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.in_port = 2
+        msg.match.dl_type = 0x0806
+        msg.actions.append(of.ofp_action_output(port=1))
+        event.connection.send(msg)
+
+        msg = of.ofp_flow_mod()
+        msg.priority = 10
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.in_port = 2
+        msg.match.dl_type = 0x0800
+        msg.actions.append(of.ofp_action_output(port=1))
+        event.connection.send(msg)
+
+    elif event.connection.dpid == s5_dpid:
+        a = packet.find('arp')
+        if a and a.protodst == "10.0.0.4":
+            msg = of.ofp_packet_out(data=event.ofp)
+            msg.actions.append(of.ofp_action_output(port=4))
+            event.connection.send(msg)
+        if a and a.protodst == "10.0.0.5":
+            msg = of.ofp_packet_out(data=event.ofp)
+            msg.actions.append(of.ofp_action_output(port=5))
+            event.connection.send(msg)
+        if a and a.protodst == "10.0.0.6":
+            msg = of.ofp_packet_out(data=event.ofp)
+            msg.actions.append(of.ofp_action_output(port=6))
+            event.connection.send(msg)
+        if a and a.protodst == "10.0.0.1":
+            msg = of.ofp_packet_out(data=event.ofp)
+            msg.actions.append(of.ofp_action_output(port=1))
+            event.connection.send(msg)
+        if a and a.protodst == "10.0.0.2":
+            msg = of.ofp_packet_out(data=event.ofp)
+            msg.actions.append(of.ofp_action_output(port=2))
+            event.connection.send(msg)
+        if a and a.protodst == "10.0.0.3":
+            msg = of.ofp_packet_out(data=event.ofp)
+            msg.actions.append(of.ofp_action_output(port=3))
+            event.connection.send(msg)
+
+        msg = of.ofp_flow_mod()
+        msg.priority = 100
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.dl_type = 0x0800
+        msg.match.nw_dst = "10.0.0.1"
+        msg.actions.append(of.ofp_action_output(port=1))
+        event.connection.send(msg)
+
+        msg = of.ofp_flow_mod()
+        msg.priority = 10
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.in_port = 6
+        msg.actions.append(of.ofp_action_output(port=3))
+        event.connection.send(msg)
+
+        msg = of.ofp_flow_mod()
+        msg.priority = 100
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.dl_type = 0x0800
+        msg.match.nw_dst = "10.0.0.1"
+        msg.actions.append(of.ofp_action_output(port=1))
+        event.connection.send(msg)
+
+        msg = of.ofp_flow_mod()
+        msg.priority = 100
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.dl_type = 0x0800
+        msg.match.nw_dst = "10.0.0.2"
+        msg.actions.append(of.ofp_action_output(port=2))
+        event.connection.send(msg)
+
+        msg = of.ofp_flow_mod()
+        msg.priority = 100
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.dl_type = 0x0800
+        msg.match.nw_dst = "10.0.0.3"
+        msg.actions.append(of.ofp_action_output(port=3))
+        event.connection.send(msg)
+
+        msg = of.ofp_flow_mod()
+        msg.priority = 100
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.dl_type = 0x0800
+        msg.match.nw_dst = "10.0.0.4"
+        msg.actions.append(of.ofp_action_output(port=4))
+        event.connection.send(msg)
+
+        msg = of.ofp_flow_mod()
+        msg.priority = 100
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.dl_type = 0x0800
+        msg.match.nw_dst = "10.0.0.5"
+        msg.actions.append(of.ofp_action_output(port=5))
+        event.connection.send(msg)
+
+        msg = of.ofp_flow_mod()
+        msg.priority = 100
+        msg.idle_timeout = 0
+        msg.hard_timeout = 0
+        msg.match.dl_type = 0x0800
+        msg.match.nw_dst = "10.0.0.6"
+        msg.actions.append(of.ofp_action_output(port=6))
+        event.connection.send(msg)
+
+
+def satisfies_intents(switch_info):
+    return True
+
+
+def handle_s1(event, packet):
+    UpdateIntent()
+    handle_s1_arp(event, packet)
+
+    direct_flow(event, "10.0.0.1", 1, False)
+    direct_flow(event, "10.0.0.2", 2, False)
+    direct_flow(event, "10.0.0.3", 3, False)
+
+    TheSwitchInfoList.sort_by_flows()
+    out_port = TheSwitchInfoList.switch_infos[0].s1_port
+    # for i in range(0, len(TheSwitchInfoList.switch_infos) - 1):
+    #     if satisfies_intents(TheSwitchInfoList.switch_infos[i]):
+    #         out_port = TheSwitchInfoList.switch_infos[i].s1_port
+    #         break
+    # print(packet.find('ip').)
+
+    print("directing flow to " + str(out_port))
+
+    direct_flow(event, "10.0.0.4", out_port, True)
+    direct_flow(event, "10.0.0.5", out_port, True)
+    direct_flow(event, "10.0.0.6", out_port, True)
+
+
+def direct_flow(event, dest_host, out_port, modify_strict):
+    msg = of.ofp_flow_mod()
+    if modify_strict:
+        msg.command = of.OFPFC_MODIFY_STRICT
+    msg.priority = 100
+    msg.idle_timeout = 0
+    msg.hard_timeout = 0
+    msg.match.dl_type = 0x0800
+    msg.match.nw_dst = dest_host
+    msg.actions.append(of.ofp_action_output(port=out_port))
+    event.connection.send(msg)
+    arp_lookup[dest_host] = out_port
+
+
+arp_lookup = {
+    "10.0.0.1": 1,
+    "10.0.0.2": 2,
+    "10.0.0.3": 3,
+    "10.0.0.4": 4,
+    "10.0.0.5": 5,
+    "10.0.0.6": 6
+}
+
+
+def handle_s1_arp(event, packet):
+    a = packet.find('arp')
+
+    if a and a.protodst == "10.0.0.4":
+        msg = of.ofp_packet_out(data=event.ofp)
+        msg.actions.append(of.ofp_action_output(port=arp_lookup["10.0.0.4"]))
+        event.connection.send(msg)
+    if a and a.protodst == "10.0.0.5":
+        msg = of.ofp_packet_out(data=event.ofp)
+        msg.actions.append(of.ofp_action_output(port=arp_lookup["10.0.0.5"]))
+        event.connection.send(msg)
+    if a and a.protodst == "10.0.0.6":
+        msg = of.ofp_packet_out(data=event.ofp)
+        msg.actions.append(of.ofp_action_output(port=arp_lookup["10.0.0.6"]))
+        event.connection.send(msg)
+    if a and a.protodst == "10.0.0.1":
+        msg = of.ofp_packet_out(data=event.ofp)
+        msg.actions.append(of.ofp_action_output(port=arp_lookup["10.0.0.1"]))
+        event.connection.send(msg)
+    if a and a.protodst == "10.0.0.2":
+        msg = of.ofp_packet_out(data=event.ofp)
+        msg.actions.append(of.ofp_action_output(port=arp_lookup["10.0.0.2"]))
+        event.connection.send(msg)
+    if a and a.protodst == "10.0.0.3":
+        msg = of.ofp_packet_out(data=event.ofp)
+        msg.actions.append(of.ofp_action_output(port=arp_lookup["10.0.0.3"]))
+        event.connection.send(msg)
+
+
+def UpdateIntent():
+    global active_intents
+    print("reading intent file!")
+    try:
+        f = open("intent.txt", "r")
+        lines = f.readlines()
+        active_intents = []
+        for line in lines:
+            arguments = line.split()
+            source_host = int(arguments[0])
+            destination_host = int(arguments[1])
+            max_delay = int(arguments[2])
+            active_intents.append(Intent(source_host, destination_host, max_delay))
+        f.close()
+        print_intents()
+    except:
+        print("failed to read intent file")
+
+
+# As usually, launch() is the function called by POX to initialize the component (routing_controller.py in our case)
+# indicated by a parameter provided to pox.py
+
+def launch():
+    global start_time
+    # core is an instance of class POXCore (EventMixin) and it can register objects.
+    # An object with name xxx can be registered to core instance which makes this object become a "component" available as pox.core.core.xxx.
+    # for examples see e.g. https://noxrepo.github.io/pox-doc/html/#the-openflow-nexus-core-openflow
+    core.openflow.addListenerByName("PortStatsReceived",
+                                    _handle_portstats_received)  # listen for port stats , https://noxrepo.github.io/pox-doc/html/#statistics-events
+    core.openflow.addListenerByName("ConnectionUp",
+                                    _handle_ConnectionUp)  # listen for the establishment of a new control channel with a switch, https://noxrepo.github.io/pox-doc/html/#connectionup
+    core.openflow.addListenerByName("PacketIn",
+                                    _handle_PacketIn)  # listen for the reception of packet_in message from switch, https://noxrepo.github.io/pox-doc/html/#packetin
